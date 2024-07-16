f7dbaa9db2f6d269cc7742a3de21bacdcd2782ba
Ashraf Abbas <ashrafabbasramadan@GMAIL.COM>
2024-07-16 19:52:06 +0300

    Add project files.

---
+++ ExcelIntergerationProject/Command.cs
@@ -0,0 +1,39 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExcelIntergerationProject
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uidoc = commandData.Application.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+            var rooms = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Rooms)
+                .WhereElementIsNotElementType()
+                .Cast<Room>()
+                .ToList();
+
+            Dictionary<string, string> roomDic = new Dictionary<string, string>();
+
+            foreach (var room in rooms)
+                roomDic.Add(room.Id.ToString(), room.Area.ToString());
+
+            ExcelService excelService = new ExcelService();
+
+            excelService.Export("RoomData",
+                                "Id",
+                                "Area",
+                                roomDic);
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ ExcelIntergerationProject/ExcelService.cs
@@ -0,0 +1,105 @@
+﻿using OfficeOpenXml;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ExcelIntergerationProject
+{
+    internal class ExcelService
+    {
+        public void Export(string Title, string ColumnHead01, string ColumnHead02, Dictionary<string, string> dic)
+        {
+            string filePath = SaveFile();
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add(Title);
+
+                // Add header row
+                worksheet.Cells[1, 1].Value = ColumnHead01;
+                worksheet.Cells[1, 2].Value = ColumnHead02;
+
+
+                int row = 2;
+
+                // Iterate through each layer group
+                foreach (var item in dic)
+                {
+                    worksheet.Cells[row, 1].Value = item.Key;
+                    worksheet.Cells[row, 2].Value = item.Value;
+                    row++;
+                }
+
+                // Adjust column widths to fit content
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+                package.SaveAs(new FileInfo(filePath));
+            }
+
+            // Open the file
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+            {
+                FileName = filePath,
+                UseShellExecute = true
+            });
+        }
+
+        public void Import(string filePath)
+        {
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add(Title);
+
+                // Add header row
+             var item01 =   worksheet.Cells[1, 1].Value;
+
+
+                int row = 2;
+
+                // Iterate through each layer group
+                foreach (var item in dic)
+                {
+                    worksheet.Cells[row, 1].Value = item.Key;
+                    worksheet.Cells[row, 2].Value = item.Value;
+                    row++;
+                }
+
+                // Adjust column widths to fit content
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+                package.SaveAs(new FileInfo(filePath));
+            }
+
+            // Open the file
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+            {
+                FileName = filePath,
+                UseShellExecute = true
+            });
+        }
+
+        private string SaveFile()
+        {
+            string filePath = null;
+            SaveFileDialog saveFileDialog = new SaveFileDialog()
+            {
+                Filter = "Excel Files (*.xlsx)|*.xlsx",
+                Title = "Select a Location to Save the Excel File",
+                FileName = "RoomArea.xlsx"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the file path from the dialog
+                filePath = saveFileDialog.FileName;
+            }
+            return filePath;
+        }
+
+    }
+}
